bb06d980a75f2bf90e68b89e2e76c96d203c23a3
Jacob D. Durrant <durrantj@pitt.edu>
2024-04-12 12:16:32 -0400

    Removing cwrap experiments.

--- src/Plugins/Optional/Webina/WebinaPlugin.vue
+++ src/Plugins/Optional/Webina/WebinaPlugin.vue
@@ -65,7 +65,6 @@ import { IQueueCallbacks } from "@/Queue/QueueTypes";
 import { getMoleculesFromStore } from "@/Store/StoreExternalAccess";
 import { isTest } from "@/Testing/SetupTests";
 import { PopupVariant } from "@/UI/Layout/Popups/InterfacesAndEnums";
-import { USE_YURI_METHOD } from "./WebinaConsts";
 
 let msgOnJobsFinishedtoUse: string | undefined;
 
@@ -625,11 +624,10 @@ export default class WebinaPlugin extends PluginParentClass {
 
         const procsPerJobBatch = webinaParams["cpu"];
 
-        // For Webina, criticial to run only one webina calculation at a time.
-        // But if not using Yuri mode, you can run multiple ligands at once, so
-        // let's do it in batches of 250.
+        // You can run multiple ligands at once, so let's do it in batches of
+        // 250.
         const simultBatches = 1;
-        const batchSize = USE_YURI_METHOD ? 1 : 250;
+        const batchSize = 250;
 
         const initialCompoundsVisible = getSetting("initialCompoundsVisible");
 
