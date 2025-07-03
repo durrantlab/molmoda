efe77398f3861d368e28c54d485d755cd2c232bd
Jacob D. Durrant <durrantj@pitt.edu>
2025-07-03 13:15:44 -0400

    All plugins can now be accessed programmatically (untested)

--- src/Plugins/Parents/PluginParentClass/Mixins/HooksMixin.ts
+++ src/Plugins/Parents/PluginParentClass/Mixins/HooksMixin.ts
@@ -6,17 +6,42 @@ import { Vue } from "vue-class-component";
  */
 export class HooksMixin extends Vue {
     /**
-     * Called right before the plugin popup opens.
+     * Called right before the plugin popup opens. Can be used to initialize the
+     * plugin's state.
      *
-     * @param  {any} payload              The payload passed to the popup if it
-     *                                    is accessed programatically.
-     * @return {Promise<boolean | void>}  If false, the popup will not open
-     *                                    (abort). Anything else, and the popup
-     *                                    will open.
+     * The default implementation supports programmatic execution. If a
+     * `payload` object containing `runProgrammatically: true` is passed, other
+     * properties of the payload are mapped to the plugin's user arguments, and
+     * the plugin's action is triggered without opening a popup. This function
+     * can also be overwritten by child plugins (and often is).
+     *
+     * @param {any} [payload] The payload passed to the plugin, used for UI
+     *        initialization or programmatic execution.
+     * @return {Promise<boolean | void>} If `false` is returned, the popup will
+     *         not open (abort).
      * @document
      */
     protected async onBeforePopupOpen(payload?: any): Promise<boolean | void> {
-        // can be optionally overridden.
+        // if payload is an object with runProgrammatically=true, set userArgs and bypass popup
+        if (
+            payload &&
+            typeof payload === "object" &&
+            !Array.isArray(payload) &&
+            payload.runProgrammatically === true
+        ) {
+            const progPayload = { ...payload };
+            delete progPayload.runProgrammatically;
+            for (const key in progPayload) {
+                if (Object.prototype.hasOwnProperty.call(progPayload, key)) {
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    this.setUserArg(key, progPayload[key]);
+                }
+            }
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this.noPopup = true;
+        }
         return;
     }
 
